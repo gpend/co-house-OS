5f771128bf3ba2e902873a82180081ee577bf54a
Greg Pendleton <gpendleton@gmail.com>
2024-10-23 20:37:49 -0500

    moving changes

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,11 +2,7 @@ import { Container } from "react-bootstrap";
 
 function Home() {
     return (
-        <Container className="text-center">
-            <h1>Welcome Home</h1>
-            <br/>
-            <p>test</p>
-        </Container>
+        <h1>Welcome Home</h1>
     )
 }
 
