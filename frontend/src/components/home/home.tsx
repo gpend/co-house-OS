5737d4d368611b75a5c1f6c6f11d1d6ade97e98b
Greg Pendleton <gpendleton@gmail.com>
2024-10-31 17:47:36 -0500

    adds beginner dockerfile, renames title, and adds to home component

--- frontend/src/components/home/home.tsx
+++ frontend/src/components/home/home.tsx
@@ -2,8 +2,19 @@ import { Container } from "react-bootstrap";
 
 function Home() {
     return (
-        <h1>Welcome Home</h1>
-    )
+        <Container>
+            <div className="text-center" id="home">
+                <h1>Welcome Home</h1>
+            </div>
+            <div>
+                <br />
+                <h2>News</h2>
+                {/*get news from api
+                <Container id="newsContainer">{getNews(5)}</Container>
+                */}
+            </div>
+        </Container>
+)
 }
 
 export default Home;
