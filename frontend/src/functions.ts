8836b1fc2dbe489681f052409be8f1a9defad65c
greg <gpendleton@gmail.com>
2024-11-01 08:27:35 -0500

    tweaks getnews function

--- frontend/src/functions.ts
+++ frontend/src/functions.ts
@@ -1,13 +1,19 @@
+var news = 
 //get
 //put
 
 
-export function getNews(count:number):any {
-    const url = `http://127.0.0.1:8223/data/news?q=${count}&appid=YOUR_API_KEY`
+export function getNews(count:number, userRole: string):any {
+    const url = `http://127.0.0.1:8223/data/news?q=${count,userRole}&appid=YOUR_API_KEY`
     fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log('fetched data:', data)
-            return(data)
+            document.getElementById("news")?.appendChild({}=>
+                <div>
+            )
         })
+        .catch{
+
+        }
 }  
